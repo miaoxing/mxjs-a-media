c1c293d674733df30ac3c42da8d0254c3c075926
twinh <twinhuang@qq.com>
2021-11-22 14:29:13 +0800

    feat(a-media): 增加 `Media` 组件

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-export default {};
+import {Box} from '@fower/react';
+import PropTypes from 'prop-types';
+
+const Media = ({children, ...props}) => {
+  return (
+    <Box flex toTop {...props}>
+      {children}
+    </Box>
+  );
+};
+
+Media.propTypes = {
+  children: PropTypes.node,
+};
+
+Media.Body = ({children, ...props}) => {
+  return (
+    <Box flex={1} ml3 {...props}>
+      {children}
+    </Box>
+  );
+};
+
+Media.Body.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Media;
